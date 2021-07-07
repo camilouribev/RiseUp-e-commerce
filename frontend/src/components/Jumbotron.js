d7138c4c1d73fa799c3e447d8e974327003c468f
Camilo U <cauribeva@unal.edu.co>
2021-07-07 01:45:43 -0500

    Agregado hash-link

--- frontend/src/components/Jumbotron.js
+++ frontend/src/components/Jumbotron.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 function Jumbotron() {
   return (
     <div>
-      <div className="jumbotron">
+      <div className="jumbotron" id="main-top">
         <div>
           <h4 className="jumbotron-text">sube tu energía</h4>
           <p className="jumbotron-text-small">
             Aprende pole dance en clases personalizadas,
             <br /> con instructoras de primer nivel.
           </p>
-          <a className="jumbo-btn" href="#plans">
+          <Link className="jumbo-btn" to="#plans">
             ver planes
-          </a>
+          </Link>
         </div>
       </div>
       <div className="jumbotron-2">
@@ -22,9 +23,9 @@ function Jumbotron() {
             los mejores insumos para disciplinas aereas ,
             <br /> en un solo lugar.
           </p>
-          <a className="jumbo-btn" href="/tienda">
+          <Link className="jumbo-btn" to="/tienda">
             ir a la tienda
-          </a>
+          </Link>
         </div>
       </div>
     </div>
